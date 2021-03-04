f33fe441960c0065ca0d10647933dc86436e8289
Egor Matyas <79442740+redh3ad@users.noreply.github.com>
2021-03-04 20:10:23 +0300

    feat
    add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
 exports.min = function min (array) {
-  return 0;
+if(arguments.length == 0) return 0;
+if(array.length == 0) return 0;
+let min = array[0];
+for (let i = 0; i < array.length; i++) {
+if (min > array[i]) min = array[i];
+}
+return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+if(arguments.length == 0) return 0;
+if(array.length == 0) return 0;
+let max = array[0];
+for (let i = 0; i < array.length; i++) {
+if (max < array[i]) max = array[i];
+}
+return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+let result = 0;
+if(arguments.length == 0) return 0;
+if(array.length == 0) return 0;
+for (let i = 0; i< array.length; i++){
+result+=array[i];
+}
+return result/array.length;
 }
